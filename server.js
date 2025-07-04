0b48c8541540cd7b78f934ac789ed1c403094a6f
ahmed <a.salawu@outlook.com>
2025-07-04 03:15:59 +0100

    Implemented fetch and delete transaction

--- server.js
+++ server.js
@@ -29,6 +29,21 @@ async function createDB() {
 
 // routes
 app.get('/', (req, res) => res.send('It is working'))
+
+app.get('/api/transactions/:userId', async (req, res) => {
+    try {
+        const {userId} =  req.params
+
+        const transactions = await sql`
+        SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+        `
+        res.status(200).json(transactions) 
+    } catch (error) {
+         console.log('Error getting the transaction', error);
+        res.status(500).send('Internal Server Error')
+    }
+})
+
 app.post('/api/transactions', async (req, res) => {
     try {
         const { title, amount, category, user_id } = req.body
@@ -47,6 +62,23 @@ app.post('/api/transactions', async (req, res) => {
         console.log('Error creating the transaction', error);
         res.status(500).send('Internal Server Error')
         
+    }
+})
+
+app.delete('/api/transactions/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const transaction = await sql`
+            DELETE FROM transactions WHERE id = ${id} RETURNING *
+        `
+
+        if (transaction.length == 0 ) {
+            return res.status(404).json({message: 'Transaction not found'})
+        }
+        res.status(200).json({message: 'Transaction deleted successfully'})
+    } catch (error) {
+        console.log('Error deleting the transaction', error);
+        res.status(500).send('Internal Server Error')
     }
 })
 
