73ec09b2e67216045f61542ef3516fa7c540ffed
ahmed <a.salawu@outlook.com>
2025-07-05 20:46:09 +0100

    cron-job added

--- src/server.js
+++ src/server.js
@@ -3,16 +3,24 @@ import dotenv from 'dotenv'
 import { createDB } from './config/db.js'
 import rateLimiter from './middlewares/rateLimiter.js'
 import transactionsRoute from './routes/transactionsRoutes.js'
+import job from './config/cron.js'
 dotenv.config()
 
 // app initialization
 const app = express()
+
+// cron-job to keep backend active
+// start cron-job only when we are in production
+if(process.env.NODE_ENV === 'production') job.start()
 
 // middleware
 app.use(express.json())
 app.use(rateLimiter)
 
 // routes
+app.get('/api/health', (req, res) => {
+    res.status(200).json({status: 'ok'})
+})
 app.use('/api/transactions', transactionsRoute)
 
 createDB().then(() =>
