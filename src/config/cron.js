006fa0ba9ccaf05bf3e6db8938d246db6332de01
ahmed <a.salawu@outlook.com>
2025-07-07 16:31:07 +0100

    cron job updated

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,7 +1,8 @@
 import cron from "cron";
+import { CronJob } from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
